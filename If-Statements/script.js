c0433504aa6e08b684784c4ce9b18fd1ffe9da9c
nurlanhuseynli2002@gmail.com <nurlanhuseynli2002@gmail.com>
2023-04-12 16:01:57 +0400

    add javascript

---
+++ If-Statements/script.js
@@ -0,0 +1,33 @@
+if(true){
+    console.log('this is true');
+}
+
+if (false) {
+    console.log('this is NOT true');
+}
+
+
+const x = 10;
+const y = 5
+
+
+if (x>=y)  {
+    console.log(`${x} boyukdur ${y} `);
+}
+
+if (x === y) {
+    console.log(`${x} beraberdir ${y}`);
+}else{
+    console.log(`${x} beraberdir deyil ${y}`);
+
+}
+
+
+if(x !== y){
+    var z = 20
+
+    console.log(`${z} is 20`);
+}
+
+console.log(z);
+
---
+++ block-scope/script.js
@@ -0,0 +1,24 @@
+const x = 100
+
+
+if(true){
+    const y = 200
+
+    console.log(x+y);
+}
+// console.log(x+y);
+
+for(let i = 0; i<=10; i++){
+    console.log(i);
+}
+
+
+if (true) {
+    const a = 5
+    let b = 3
+    var c = 6
+
+    console.log(a+b+c);
+}
+
+// console.log(c);
