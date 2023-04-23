f09e031c4caf05755400dbe9b7ab868285c7b035
nurlanhuseynli2002@gmail.com <nurlanhuseynli2002@gmail.com>
2023-04-23 13:28:53 +0400

    add

---
+++ Document Element Properties/script.js
@@ -0,0 +1,35 @@
+let output;
+
+// document.all is deprecated
+output = document.all;
+output = document.all[11];
+output = document.all.length;
+
+// Everything in the html tags
+output = document.documentElement;
+
+// Head and body tags
+output = document.head;
+output = document.body;
+
+// HTMLCollection of everything in head/body
+output = document.head.children;
+output = document.body.children;
+
+// Random properties
+output = document.doctype;
+output = document.domain;
+output = document.URL;
+output = document.characterSet;
+output = document.contentType;
+
+// Get all forms
+output = document.forms;
+output = document.forms[0];
+output = document.forms[0].id;
+output = document.forms[0].method;
+output = document.forms[0].action;
+
+console.log(output);
+// Change a form id
+document.forms[0].id = 'new-id';
